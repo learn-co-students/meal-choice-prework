98756767b99f53ba2d544b64021f7091b96b2bac
github username <crishanks12@gmail.com>
2018-10-23 23:53:14 -0600

    added practice test case

--- meal_choice.rb
+++ meal_choice.rb
@@ -5,3 +5,11 @@ end
 meal_choice('cheese')
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
+
+describe "meal_choice" do
+  it "returns 'cheese' when passed in cheese, or returns 'meat' when nothing is passed in" do
+    pass_in_cheese = meal_choice('cheese')
+
+    expect(pass_in_cheese).to eq('cheese')
+  end
+end
